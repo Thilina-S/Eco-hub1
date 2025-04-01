cce9df2a5d02e6cd0b82e02a08267926a2cf2e2a
thilinasandamal <thilinasandamal61@gmail.com>
2025-04-01 23:53:48 +0530

    create the DB connection

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import cors from "cors";
+
+dotenv.config();
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Connect to MongoDB
+connectDB();
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
--- feed/src/components/pages/Marketplace.jsx
+++ feed/src/components/pages/Marketplace.jsx
@@ -218,4 +218,4 @@ export default function ProductGrid() {
       </div>
     </div>
   );
-}
+} ba
